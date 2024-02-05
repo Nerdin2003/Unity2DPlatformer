92a1ef065a8bee5dbdd325840f8ca571c27dd678
Nerdin2003 <73284307+Nerdin2003@users.noreply.github.com>
2024-02-05 09:48:19 +0200

    Add files via upload

---
+++ Platformer/CollectSoul.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectSoul : MonoBehaviour
+{
+    void Start()
+{
+ScoringSystem.theScore = 0;
+}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        ScoringSystem.theScore += 1;
+        Destroy(gameObject);
+    }
+
+}
---
+++ Platformer/MoveingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveingPlatform : MonoBehaviour
+{
+    float dirX;
+    float speed = 3f;
+
+    bool moveingRight = true;
+
+    void Update()
+    {
+        if (transform.position.x > 70f)
+        {   
+            moveingRight = false;
+        }
+        //
+        else if (transform.position.x < 65f)
+        {
+            moveingRight = true;
+        }
+
+        if (moveingRight)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
